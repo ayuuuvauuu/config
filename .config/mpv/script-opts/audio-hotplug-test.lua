4fa1ad52743bd6c16727fc470ae97e6f74798bfb
ayush mishra <99461867+ayuuuvauuu@users.noreply.github.com>
2026-01-23 20:02:26 +0530

    a: mpv quality menu

--- .config/mpv/script-opts/audio-hotplug-test.lua
+++
@@ -1,6 +0,0 @@
-mp.observe_property("audio-device-list", "native", function(_, val)
-    print("Audio device list changed:")
-    for _, e in ipairs(val) do
-        print("  - '" .. e.name .. "' (" .. e.description .. ")")
-    end
-end)
