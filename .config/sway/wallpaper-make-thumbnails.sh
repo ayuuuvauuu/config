f5e0a841343fd99920f17a68554decaaab456527
ayush mishra <99461867+ayuuuvauuu@users.noreply.github.com>
2026-05-18 12:05:24 +0530

    m: parallaized thumbnail maker

--- .config/sway/wallpaper-make-thumbnails.sh
+++ .config/sway/wallpaper-make-thumbnails.sh
@@ -1,16 +1,12 @@
-# 1. Clear existing thumbnail cache
 rm -rf ~/.cache/rofi-thumbs
 mkdir -p ~/.cache/rofi-thumbs
-
-# 2. Go to wallpaper directory
 cd ~/Pictures/wallpapers || exit 1
 
-# 3. Generate thumbnails with aspect ratio preserved (256px max)
+export OUTDIR="$HOME/.cache/rofi-thumbs"
+
 find . -type f \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' -o -iname '*.webp' \) -print0 |
-while IFS= read -r -d '' img; do
-    name="$(basename "$img")"
-    ffmpeg -y -loglevel error \
-        -i "$img" \
-        -vf "scale=256:256:force_original_aspect_ratio=decrease" \
-        "$HOME/.cache/rofi-thumbs/$name"
-done
+  parallel -0 -j+0 --bar '
+    img={}
+    name=$(basename "$img")
+    ffmpeg -y -loglevel error -i "$img" -vf "scale=256:256:force_original_aspect_ratio=decrease" "$OUTDIR/$name"
+  '
