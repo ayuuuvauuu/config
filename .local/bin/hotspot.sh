cc34fad9798439a82dbd7840dbff8cdcb6813eab
ayu <ayu@local>
2026-08-14 22:42:43 +0530

    f

--- .local/bin/hotspot.sh
+++ .local/bin/hotspot.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-sudo create_ap -c 10 wlan0 wlan0 victus password
+IFACE="wlan0"
+FREQ=$(iw dev "$IFACE" link | sed -n 's/.*freq: \([0-9]*\).*/\1/p')
+if [ -z "$FREQ" ]; then
+    CHANNEL=6
+elif [ "$FREQ" -ge 5000 ]; then
+    CHANNEL=$(( (FREQ - 5000) / 5 ))
+else
+    CHANNEL=$(( (FREQ - 2407) / 5 ))
+fi
+sudo create_ap -c "$CHANNEL" "$IFACE" "$IFACE" victus password
